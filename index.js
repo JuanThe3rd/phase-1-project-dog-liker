a3d1eb01392d691e741d43a5b71244bd61b7c22b
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-06-06 16:25:30 -0700

    adds db to DOM and fixes some CSS

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-//test change
+const dogAPI = 'http://localhost:3000/dogs';
+const dogCollection = document.getElementById('dog-collection')
+
+fetch(dogAPI)
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(renderDog);
+    })
+
+function renderDog(dog){
+    const newDog = document.createElement('div');
+    newDog.classList.add('dog-card');
+
+    newDog.innerHTML = `
+        <h2 class="dog-card-content">${dog.name}</h2>
+        <img class ="dog-image" src="${dog.image}" alt="${dog.name}IMG">
+        <p class="dog-card-content">${dog.breed}</p>
+        <p class="dog-card-content">${dog.likes} Likes</p>
+        <button>Like ❤️</button>
+    `
+
+    dogCollection.append(newDog);
+}
