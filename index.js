b30d6a0f2dcbe808fa7a1bb4c0cde4e4142d3397
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-06-09 09:00:42 -0700

    adds more css

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function renderDog(dog){
         <img class ="dog-image" src="${dog.image}" alt="${dog.name}IMG">
         <p class="dog-card-content">Breed: ${dog.breed}</p>
         <p class="dog-card-content">${dog.likes} Likes</p>
-        <button title="Like" class="like-btn" id="${likeButtonId}">Like❤️</button>
+        <button title="Like" class="like-btn" id="${likeButtonId}">Like 👍</button>
     `;
     dogCollection.append(newDog);
 
